c6f207100ade43bd37d55f769974991c1a602758
tilenpirih <33714934+tilenpirih@users.noreply.github.com>
2018-10-05 11:40:13 +0200

    Delete krog.py

--- vaje1/krog.py
+++
@@ -1,7 +0,0 @@
-#Copyright by Tilen Pirih
-from math import *
-polmer=float(input("Polmer kroga?"))
-obseg_kroga=2*pi*polmer
-ploscina_kroga=pi*polmer**2
-print("Obseg kroga: ", obseg_kroga)
-print("Ploscina kroga: ",ploscina_kroga)
