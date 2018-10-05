c27b9d2b6495ece0fe5388b461cfcd7c022029fe
tilenpirih <33714934+tilenpirih@users.noreply.github.com>
2018-10-05 11:40:24 +0200

    Delete pitagorov_izrek.py

--- vaje1/pitagorov_izrek.py
+++
@@ -1,8 +0,0 @@
-#Copyright by Tilen Pirih
-from math import *
-
-kateta1=float(input("kateta 1?"))
-kateta2=float(input("kateta 2?"))
-hipotenuza=sqrt(pow(kateta1,2)+pow(kateta2,2))
-print("Hipotenuza je ",hipotenuza)
-
