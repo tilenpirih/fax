054cc3ed026c51a3a623df46404bcaa612277058
tilenpirih <33714934+tilenpirih@users.noreply.github.com>
2018-10-05 11:39:50 +0200

    Delete hitri_prst.py

--- vaje1/hitri_prst.py
+++
@@ -1,7 +0,0 @@
-#Copyright by Tilen Pirih
-from math import *
-import time
-start = time.time()
-st=float(input("Koliko je 6*7?"))
-end = time.time()
-print("Za razmišljanje ste porabili" ,end-start,"s")
