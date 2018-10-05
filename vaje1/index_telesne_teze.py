2721a9f01ed5c7337c1cbc02c02cc8545a538298
tilenpirih <33714934+tilenpirih@users.noreply.github.com>
2018-10-05 11:40:08 +0200

    Delete index_telesne_teze.py

--- vaje1/index_telesne_teze.py
+++
@@ -1,7 +0,0 @@
-#Copyright by Tilen Pirih
-from math import *
-
-teza=float(input("Vnesi tezo [kg]"))
-visina=float(input("Vnesi velikost [cm]"))
-itm=teza/(visina/100)**2
-print("Index telesne mase je: ",itm)
